669420607057014750f980513420d4fd58548f15
Shoaib <shoaibmalek21@gmail.com>
2026-06-22 01:38:59 +0530

    commit

---
+++ aiohttp-scraper/scraper.py
@@ -0,0 +1,120 @@
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import csv
+
+async def scrape_quotes():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://quotes.toscrape.com") as response:
+            # Access the HTML of the target page
+            html = await response.text()
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(html, "html.parser")
+            
+            # Where to store the scraped data
+            quotes = []
+
+            # Extract all quotes from the page
+            quote_elements = soup.find_all("div", class_="quote")
+
+            # Loop through quotes and extract text, author, and tags
+            for quote_element in quote_elements:
+                text = quote_element.find("span", class_="text").get_text().replace("“", "").replace("”","")
+                author = quote_element.find("small", class_="author")
+                tags = [tag.get_text() for tag in quote_element.find_all("a", class_="tag")]
+
+                # Store the scraped data
+                quotes.append({
+                    "text": text,
+                    "author": author,
+                    "tags": tags
+                })
+                
+            # Open the file for export
+            with open("quotes.csv", mode="w", newline="", encoding="utf-8") as file:
+                writer = csv.DictWriter(file, fieldnames=["text", "author", "tags"])
+
+                # Write the header row
+                writer.writeheader()
+
+                # Write the scraped quotes data
+                writer.writerows(quotes)
+
+async def fetch_with_custom_headers():
+    # Custom headers for the request
+    headers = {
+        "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7,es-US;q=0.6,es;q=0.5,it-IT;q=0.4,it;q=0.3"
+    }
+
+    async with aiohttp.ClientSession() as session:
+        # Make a GET request with custom headers
+        async with session.get("https://httpbin.io/anything", headers=headers) as response:
+            data = await response.json()
+            # Handle the response...
+            print(data)
+
+
+async def fetch_with_custom_user_agent():
+    # Define a Chrome-like custom User-Agent
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+    }
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        # Make a GET request with the custom User-Agent
+        async with session.get("https://httpbin.io/anything") as response:
+            data = await response.text()
+            # Handle the response...
+            print(data)
+
+
+async def fetch_with_custom_cookies():
+    # Define cookies as a dictionary
+    cookies = {
+        "session_id": "9412d7hdsa16hbda434shhnaj",
+        "user_preferences": "dark_mode=false"
+    }
+
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        # Make a GET request with custom cookies
+        async with session.get("https://httpbin.io/anything") as response:
+            data = await response.text()
+            # Handle the response...
+            print(data)
+
+async def fetch_through_proxy():
+    # Replace with the URL of your proxy server
+    proxy_url = "<YOUR_PROXY_URL>"
+
+    async with aiohttp.ClientSession() as session:
+        # Make a GET request through the proxy server
+        async with session.get("https://httpbin.io/anything", proxy=proxy_url) as response:
+            data = await response.text()
+            # Handle the response...
+            print(data)
+
+# from aiohttp_retry import RetryClient, ExponentialRetry
+
+# async def main():
+#     retry_options = ExponentialRetry(attempts=1)
+#     retry_client = RetryClient(raise_for_status=False, retry_options=retry_options)
+#     async with retry_client.get("https://httpbin.io/anything") as response:
+#         print(response.status)
+
+#     await retry_client.close()
+
+# Run the event loop
+# asyncio.run(fetch_through_proxy())
+
+# # Run the event loop
+# asyncio.run(fetch_with_custom_cookies())
+
+# # Run the event loop
+# asyncio.run(fetch_with_custom_user_agent())
+
+# # Run the event loop
+# asyncio.run(fetch_with_custom_headers())
+
+# Run the asynchronous function
+asyncio.run(scrape_quotes())
